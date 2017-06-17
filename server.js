8378f9e334e291d8958d095619525a1095856fcc
Benga AKingbe <g.akingbe@gmail.com>
2017-06-17 00:33:56 -0400

    changed scraped site to NY times

--- server.js
+++ server.js
@@ -46,37 +46,38 @@ db.once("open", function() {
 // A GET request to scrape the reddit/r/news website
 app.get("/scrape", function(req, res) {
 	// First we grab the body of the html with request
-	request("http://www.reddit.com/r/news", function(error, response, html) {
+	request("http://www.nytimes.com/", function(error, response, html) {
 		//Then, we load that into cheerio and save it to $ for a shorthand selector
 		console.log("made request with cheerio")
 
 		var $ = cheerio.load(html);
 		//Now, we grab every h2 within an article tag, and do the following:
-		$("span.domain").each(function(i, element) {
+		$("h2.story-heading").each(function(i, element) {
+			// var a = $(this).prev();
 			console.log("iterating through h2")
-			// Save an empty result object
-			// var result = {};
+			//Save an empty result object
+			var result = {};
 
-			// //Add the text and href of every link, and save them as properties of the result object
-			// result.title = $(this).children("a").text();
-			// result.link = $(this).children("a").attr("href");
-			// console.log(result);
+			//Add the text and href of every link, and save them as properties of the result object
+			result.title = $(this).children("a").text();
+			result.link = $(this).children("a").attr("href");
+			console.log(result);
 
-			// //Using our Article model, create a new entry
-			// //This effectively passes the result object to the entry (and the title and link)
-			// var entry = new Article(result);
+			//Using our Article model, create a new entry
+			//This effectively passes the result object to the entry (and the title and link)
+			var entry = new Article(result);
 
-			// //Now, save that entry to the db
-			// entry.save(function(err, doc) {
-			// 	//Log any errors
-			// 	if(err) {
-			// 		console.log(err);
-			// 	}
-			// 	// Or log the doc
-			// 	else {
-			// 		res.json(doc);
-			// 	}
-			// })
+			//Now, save that entry to the db
+			entry.save(function(err, doc) {
+				//Log any errors
+				if(err) {
+					console.log(err);
+				}
+				// Or log the doc
+				else {
+					res.json(doc);
+				}
+			})
 		})
 	});
 	// Tell the browser that we finished scraping the text
